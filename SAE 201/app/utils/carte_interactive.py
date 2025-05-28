30163f359b66db18c71b90d5429b3fd13dd165dc
Ylan-L <leangylan@gmail.com>
2025-05-28 16:56:16 +0200

    carte de france

--- SAE 201/app/utils/carte_interactive.py
+++ SAE 201/app/utils/carte_interactive.py
@@ -1,13 +1,23 @@
 import folium
+import requests
 
-# Créer une carte sans fond (pas de tuiles)
-carte = folium.Map(
-    location=[46.5, 2.2],  # Centre de la France
-    zoom_start=6,
-    tiles=None  # Aucune tuile (carte blanche)
-)
+# Charger les données GeoJSON de la France depuis GitHub
+url = "https://raw.githubusercontent.com/gregoiredavid/france-geojson/master/regions-version-simplifiee.geojson"
+geojson_data = requests.get(url).json()
 
-# Optionnel : ajouter une forme ou un marqueur
-folium.Marker([48.8566, 2.3522], popup="Paris").add_to(carte)
+# Créer une carte blanche sans tuiles de fond
+carte = folium.Map(location=[46.5, 2.2], zoom_start=5, tiles=None)
 
+# Ajouter les contours en noir
+folium.GeoJson(
+    geojson_data,
+    style_function=lambda feature: {
+        "fillColor": "white",
+        "color": "black",
+        "weight": 1,
+        "fillOpacity": 1,
+    }
+).add_to(carte)
+
+# Sauvegarder la carte
 carte.save("carte.html")
